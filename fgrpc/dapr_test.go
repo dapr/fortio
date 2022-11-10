628f6297bc0152832633a07ec88e9d04090a663c
Shubham Sharma <shubhash@microsoft.com>
2022-11-10 16:48:49 +0530

    Add publish unit tests
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- fgrpc/dapr_test.go
+++ fgrpc/dapr_test.go
@@ -1,8 +1,9 @@
 package fgrpc
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestParseDaprParameters4InvokeAppCallback(t *testing.T) {
@@ -29,4 +30,116 @@ func TestParseDaprParameters4InvokeDapr(t *testing.T) {
 	assert.Equal(t, d.params.appId, "testapp")
 	assert.Equal(t, d.params.extensions["ex1"], "1")
 	assert.Equal(t, d.params.extensions["ex2"], "2")
+}
+
+func TestPrepareRequest4PubSub(t *testing.T) {
+	t.Run("params sanity test", func(t *testing.T) {
+		testcases := []struct {
+			name    string
+			params  string
+			isError bool
+		}{
+			{
+				name:    "method is missing",
+				params:  "capability=pubsub,target=dapr,store=memstore,topic=mytopic",
+				isError: true,
+			},
+			{
+				name:    "store is missing",
+				params:  "capability=pubsub,target=dapr,method=publish,topic=mytopic",
+				isError: true,
+			},
+			{
+				name:    "topic is missing",
+				params:  "capability=pubsub,target=dapr,method=publish,store=memstore",
+				isError: true,
+			},
+			{
+				name:    "method is invalid",
+				params:  "capability=pubsub,target=dapr,method=invalid,store=memstore,topic=mytopic",
+				isError: true,
+			},
+			{
+				name:    "when method is bulkpublish, numevents is missing",
+				params:  "capability=pubsub,target=dapr,method=bulkpublish,store=memstore,topic=mytopic",
+				isError: true,
+			},
+			{
+				name:    "when method is bulkpublish, numevents is invalid",
+				params:  "capability=pubsub,target=dapr,method=bulkpublish,store=memstore,topic=mytopic,numevents=invalid",
+				isError: true,
+			},
+			{
+				name:    "valid with method publish",
+				params:  "capability=pubsub,target=dapr,method=publish,store=memstore,topic=mytopic",
+				isError: false,
+			},
+			{
+				name:    "valid with method bulkpublish",
+				params:  "capability=pubsub,target=dapr,method=bulkpublish,store=memstore,topic=mytopic,numevents=100",
+				isError: false,
+			},
+		}
+
+		for _, tc := range testcases {
+			t.Run(tc.name, func(t *testing.T) {
+				o := &GRPCRunnerOptions{}
+				o.UseDapr = tc.params
+
+				d := &DaprGRPCRunnerResults{}
+				err := d.parseDaprParameters(o.UseDapr)
+				assert.NoError(t, err, "parse failed")
+
+				err = d.prepareRequest4PubSub(o)
+				if tc.isError {
+					assert.Error(t, err, "should fail")
+				} else {
+					assert.NoError(t, err, "should succeed")
+				}
+			})
+		}
+	})
+
+	t.Run("publish request", func(t *testing.T) {
+		o := &GRPCRunnerOptions{}
+		o.UseDapr = "capability=pubsub,target=dapr,method=publish,store=memstore,topic=mytopic,contenttype=text/plain"
+		o.Payload = "hello world"
+
+		d := &DaprGRPCRunnerResults{}
+		err := d.parseDaprParameters(o.UseDapr)
+		assert.NoError(t, err, "parse failed")
+
+		err = d.prepareRequest4PubSub(o)
+		assert.NoError(t, err, "prepare failed")
+
+		assert.Equal(t, "memstore", d.publishEventRequest.PubsubName)
+		assert.Equal(t, "mytopic", d.publishEventRequest.Topic)
+		assert.Equal(t, "text/plain", d.publishEventRequest.DataContentType)
+		assert.Equal(t, "hello world", string(d.publishEventRequest.Data))
+
+		assert.Len(t, d.publishEventRequests, 0)
+	})
+
+	t.Run("bulk publish request", func(t *testing.T) {
+		o := &GRPCRunnerOptions{}
+		o.UseDapr = "capability=pubsub,target=dapr,method=bulkpublish,store=memstore,topic=mytopic,contenttype=text/plain,numevents=100"
+		o.Payload = "hello world"
+
+		d := &DaprGRPCRunnerResults{}
+		err := d.parseDaprParameters(o.UseDapr)
+		assert.NoError(t, err, "parse failed")
+
+		err = d.prepareRequest4PubSub(o)
+		assert.NoError(t, err, "prepare failed")
+
+		assert.Len(t, d.publishEventRequests, 100)
+		for _, req := range d.publishEventRequests {
+			assert.Equal(t, "memstore", req.PubsubName)
+			assert.Equal(t, "mytopic", req.Topic)
+			assert.Equal(t, "text/plain", req.DataContentType)
+			assert.Equal(t, "hello world", string(req.Data))
+		}
+
+		assert.Nil(t, d.publishEventRequest)
+	})
 }
